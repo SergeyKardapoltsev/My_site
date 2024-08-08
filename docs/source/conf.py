42df84913cd4a48a8367e9e8278c1bb7bfd47e0b
Sergey Kardapoltsev <kardapoltsev@list.ru>
2024-08-08 19:53:21 +0300

    Исправить ошибку, добавив тему в расширения

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'SergeyKardapoltsev'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['python_docs_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
